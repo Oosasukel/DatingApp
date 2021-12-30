e9bd015b624814cbb035b8263caf1615a286db2b
Oosasukel <rodrigo_gonn@hotmail.com>
2021-12-30 00:27:16 -0300

    Repostiory and mapper

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(
+                    memberDto => memberDto.PhotoUrl,
+                    opt => opt.MapFrom(
+                        appUser => appUser.Photos.FirstOrDefault(photo => photo.IsMain).Url
+                    )
+                );
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
