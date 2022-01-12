02f2692d9d5c76e5aae58a2fd5d8a1e4794b6c4b
Oosasukel <rodrigo_gonn@hotmail.com>
2022-01-12 12:28:37 -0300

    upload, update and remove photos

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -51,7 +51,9 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.Username == loginDto.Username.ToLower());
+            var user = await _context.Users
+                .Include(u => u.Photos)
+                .SingleOrDefaultAsync(x => x.Username == loginDto.Username.ToLower());
 
             if (user == null) return Unauthorized("Invalid username");
 
@@ -68,7 +70,8 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.Username,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(photo => photo.IsMain)?.Url
             };
         }
 
